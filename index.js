48d86087cb6c3bd04f1d0c3db353ed10a969bd59
yasar2385 <yasar.mohideen@newgen.co>
2022-07-02 23:44:59 +0530

    2 files modified

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-ar categoryItems = document.querySelectorAll("[data-category-group]");
+var categoryItems = document.querySelectorAll('[data-category-group]');
 var categoryItemsArray = Array.from(categoryItems);
 
 let sorted = categoryItemsArray.sort(sorter);
 
-function sorter(a,b) {
-    return a.dataset.categoryGroup.localeCompare(b.dataset.categoryGroup); // sorts based on alphabetical order
+function sorter(a, b) {
+  return a.dataset.categoryGroup.localeCompare(b.dataset.categoryGroup); // sorts based on alphabetical order
 }
 
-document.querySelector("button").onclick = () => sorted.forEach(e => document.querySelector("#demo").appendChild(e));
+document.querySelector('button').onclick = () =>
+  sorted.forEach((e) => document.querySelector('#demo').appendChild(e));
+
+let j = 30;
+let IsRefAvail = document.querySelectorAll(`a.xref[rid*="${j}"]`).length != 0;
+console.log(IsRefAvail);
