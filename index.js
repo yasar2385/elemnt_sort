19247e1e60c7aa96fdf48a0d24ba0d515ca06b6b
yasar2385 <yasar.mohideen@nkw.pub>
2022-08-30 19:49:48 +0530

    3 files modified

--- index.js
+++ index.js
@@ -20,3 +20,38 @@ document.querySelector('button').onclick = () =>
 let j = 30;
 let IsRefAvail = document.querySelectorAll(`a.xref[rid*="${j}"]`).length != 0;
 console.log(IsRefAvail);
+
+var ARRAY_DEMO = [
+  {
+    Original_lab: 'Figure 5',
+    Original_Id: 'F5',
+    Seq_No: '5',
+  },
+  {
+    Original_lab: 'Figure 1:',
+    Original_Id: 'F1',
+    Seq_No: '1:',
+  },
+  {
+    Original_lab: 'Figure 2:',
+    Original_Id: 'F2',
+    Seq_No: '2:',
+  },
+  {
+    Original_lab: 'Figure 3:',
+    Original_Id: 'F3',
+    Seq_No: '3:',
+  },
+  {
+    Original_lab: 'Figure 4:',
+    Original_Id: 'F4',
+    Seq_No: '4:',
+  },
+];
+
+ARRAY_DEMO.sort(function (a, b) {
+  if (a.Seq_No > b.Seq_No) return 1;
+  if (a.Seq_No < b.Seq_No) return -1;
+  return 0;
+});
+console.log(ARRAY_DEMO);
